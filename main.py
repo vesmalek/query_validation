a32f22a3db507d601640b982c3f3e3d922fe86a4
Ismail Malik <vesmalick@gmail.com>
2026-06-08 11:24:14 +0300

    Completed task 01

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Hello, world!")
+from typing import Annotated
+from fastapi import FastAPI, Query
+
+app = FastAPI()
+
+@app.get("/search")
+async def search(
+    q: Annotated[str | None, Query(
+        title='Search query',
+        description='Keyword to search products by name',
+        min_length=2,
+        max_length=100
+    )] = None
+):
+    result = {"searching_for": q} if q else {"result": "no search term"}
+    return result
