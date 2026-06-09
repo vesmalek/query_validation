826c1f2c26f7f5a8591dc2f85ae1a3f2885c52b9
Ismail Malik <vesmalick@gmail.com>
2026-06-09 22:16:41 +0300

    completed task 02 path num validation

--- path_num_validation.py
+++ path_num_validation.py
@@ -3,6 +3,8 @@ from typing import Annotated
 
 app = FastAPI()
 
+products = ["shirt", "shoes", "hat", "belt", "socks", "watch", "bag", "jacket", "scarf", "gloves"]
+
 @app.get("/products/{product_id}")
 async def get_product(
     product_id: Annotated[int, Path(
@@ -12,3 +14,17 @@ async def get_product(
     )]
 ):
     return {'product_id': product_id}
+
+@app.get("/products")
+async def get_products(
+    skip: Annotated[int, Query(
+        ge=0
+    )] = 0,
+    limit: Annotated[int, Query(
+        ge=1,
+        le=50
+    )] = 10
+):
+    return products[skip: skip + limit]
+
+    
